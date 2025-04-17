79fcbcc9ec1ead37f78c10647a96bcab658b9b08
Daniil <novinskijdana77@gmail.com>
2025-04-18 02:15:47 +0300

    0.003v
    добавлены методы для админ панели, пока что без реализации

--- src/main/java/org/telegramBotStructure/adminFunctions/AdminCommands.java
+++ src/main/java/org/telegramBotStructure/adminFunctions/AdminCommands.java
@@ -1,4 +1,15 @@
 package org.telegramBotStructure.adminFunctions;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 public interface AdminCommands {
+    public SendMessage adminCommands();
+    public SendMessage addSubjects();
+    public SendMessage removeSubjects();
+    public SendMessage addHomework();
+    public SendMessage removeHomework();
+    public SendMessage addMailing();
+    public SendMessage removeMailing();
+    public SendMessage addTimetable();
+    public SendMessage removeTimetable();
 }
--- src/main/java/org/telegramBotStructure/adminFunctions/AdminPanel.java
+++ src/main/java/org/telegramBotStructure/adminFunctions/AdminPanel.java
@@ -1,4 +1,51 @@
 package org.telegramBotStructure.adminFunctions;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 public class AdminPanel implements AdminCommands{
+
+    @Override
+    public SendMessage adminCommands() {
+        return null;
+    }
+
+    @Override
+    public SendMessage addSubjects() {
+        return null;
+    }
+
+    @Override
+    public SendMessage removeSubjects() {
+        return null;
+    }
+
+    @Override
+    public SendMessage addHomework() {
+        return null;
+    }
+
+    @Override
+    public SendMessage removeHomework() {
+        return null;
+    }
+
+    @Override
+    public SendMessage addMailing() {
+        return null;
+    }
+
+    @Override
+    public SendMessage removeMailing() {
+        return null;
+    }
+
+    @Override
+    public SendMessage addTimetable() {
+        return null;
+    }
+
+    @Override
+    public SendMessage removeTimetable() {
+        return null;
+    }
 }
