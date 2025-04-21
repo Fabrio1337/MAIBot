73656feff58fec898fb60ad7fd0b6f548d4a18b1
Daniil <68847000+Fabrio1337@users.noreply.github.com>
2025-04-21 15:30:21 +0300

    0.0057v
    добавлена реализация добавления данных в БД

--- src/main/java/org/telegramBotStructure/DatabaseDAO/DatabaseMethodsImpl.java
+++ src/main/java/org/telegramBotStructure/DatabaseDAO/DatabaseMethodsImpl.java
@@ -1,8 +1,11 @@
 package org.telegramBotStructure.DatabaseDAO;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.telegramBotStructure.entity.*;
 
 @Repository
@@ -13,8 +16,19 @@ public class DatabaseMethodsImpl implements DatabaseMethods{
 
 
     @Override
+    @Transactional
     public Admin getAdmin(long chatId) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        Admin admin = session.createQuery("from Admin admin where userId= :chatId", Admin.class)
+                .setParameter("chatId", chatId)
+                .uniqueResult();
+        if(admin == null)
+        {
+            return null;
+        }
+        else {
+            return admin;
+        }
     }
 
     @Override
@@ -53,37 +67,51 @@ public class DatabaseMethodsImpl implements DatabaseMethods{
     }
 
     @Override
+    @Transactional
     public void setAdmin(Admin admin) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(admin);
     }
 
     @Override
+    @Transactional
     public void setSubject(Subject subject) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(subject);
     }
 
     @Override
+    @Transactional
     public void setUser(User user) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(user);
     }
 
     @Override
+    @Transactional
     public void setGroup(MaiGroup maiGroup) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(maiGroup);
     }
 
     @Override
+    @Transactional
     public void setMailing(Mailing mailing) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(mailing);
     }
 
     @Override
+    @Transactional
     public void setHomework(Homework homework) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(homework);
     }
 
     @Override
+    @Transactional
     public void setSchedule(Schedule schedule) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.merge(schedule);
     }
 }
